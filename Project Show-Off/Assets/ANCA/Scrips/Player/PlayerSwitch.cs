2539a6cf0c949f8ec3f08d16e449866cc008beab
anca6 <526100@student.saxion.nl>
2024-05-14 13:33:42 +0200

    Improved character switching with cooldown

--- Project Show-Off/Assets/ANCA/Scrips/Player/PlayerSwitch.cs
+++ Project Show-Off/Assets/ANCA/Scrips/Player/PlayerSwitch.cs
@@ -4,40 +4,50 @@ using UnityEngine;
 
 public class PlayerSwitch : MonoBehaviour
 {
-    public Transform characterTransform;
-    public List<Transform> possibleCharacters;
-    public int currentCharacter;
+    private Transform characterTransform;
+    private int currentCharacter;
+
+    private float lastSwitchTime = 0f;
+    private float switchCooldown = 1f;
+
+    [SerializeField] private List<Transform> possibleCharacters;
 
     private void Start()
     {
         if (characterTransform == null && possibleCharacters.Count >= 1)
         {
             characterTransform = possibleCharacters[0];
+            currentCharacter = 0;
+            characterTransform.gameObject.SetActive(true);
         }
     }
 
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Alpha1))
+        if (Time.time - lastSwitchTime < switchCooldown)
         {
-            currentCharacter = 0;
-            Debug.Log("switched to 1");
-            Switch();
+            return;
         }
-        else if (Input.GetKeyUp(KeyCode.Alpha2))
-        {
-            currentCharacter = 1;
-            Debug.Log("switched to 2");
-            Switch();
-        }
-        else if (Input.GetKeyUp(KeyCode.Alpha3))
-        {
-            currentCharacter = 2;
-            Debug.Log("switched to 3");
-            Switch();
-        }
-    }
+            int newCharacter = currentCharacter;
 
+            switch (true)
+            {
+                case true when Input.GetKeyUp(KeyCode.Alpha1):
+                    newCharacter = 0; break;
+                case true when Input.GetKeyUp(KeyCode.Alpha2):
+                    newCharacter = 1; break;
+                case true when Input.GetKeyUp(KeyCode.Alpha3):
+                    newCharacter = 2; break;
+            }
+
+            if (newCharacter != currentCharacter)
+            {
+                currentCharacter = newCharacter;
+                Debug.Log($"switched to {currentCharacter + 1}");
+                lastSwitchTime = Time.time;
+                Switch();
+            }
+    }
 
     private void Switch()
     {
