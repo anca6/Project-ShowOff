75868365a472f04036e4568a5fefdcaad6aabc73
anca6 <526100@student.saxion.nl>
2024-05-27 12:55:40 +0200

    Jojo - hardcoded standing up method (will be revisited)

--- Project Show-Off/Assets/ANCA/Scrips/Player/Jojo.cs
+++ Project Show-Off/Assets/ANCA/Scrips/Player/Jojo.cs
@@ -10,6 +10,7 @@ public class Jojo : Character
     [SerializeField] private GameObject explosionEffect;
     [SerializeField] float minBreakSpeed = 10;
 
+    bool StandupFinished = true;
 
 
     protected override void Start()
@@ -22,33 +23,46 @@ public class Jojo : Character
         playerControls = new PlayerControls();
         playerControls.Gameplay.Jump.performed += ctx => Jump();
     }
- 
+
     void StandupStraight()
     {
         //rb.MoveRotation(Quaternion.identity); // maybe this works?
-        Debug.Log("Jojo stands up");
-        StartCoroutine(StandupRoutine());
+        if (StandupFinished && !Input.GetKey(KeyCode.LeftShift))
+        {
+            Debug.Log("Jojo stands up"); // TODO: work with state machine
+            StartCoroutine(StandupRoutine());
+        }
     }
+
 
     IEnumerator StandupRoutine()
     {
+        StandupFinished = false;
         Quaternion startRotation = transform.rotation;
-        for (int i=0;i<=20;i++) // TODO: Maybe make framerate independent
+        Vector3 lookDir = rb.velocity.magnitude > 0 ? rb.velocity.normalized : Vector3.forward;
+
+        for (int i=0;i<=20;i++) // TODO: Maybe make framerate independent: use delta time
         {
-            rb.MoveRotation(Quaternion.Slerp(startRotation, Quaternion.identity, i / 20f));
+            Debug.Log("Delta time: " + Time.deltaTime);
+            rb.MoveRotation(Quaternion.Slerp(startRotation, Quaternion.LookRotation(lookDir), i / 20f));
+
             yield return new WaitForEndOfFrame();
         }
+        StandupFinished = true;
     }
 
     protected override void Movement()
     {
+        //TO DO: Add rolling state machine here
+
         rb.constraints = RigidbodyConstraints.None;
 
         if (IsGrounded())
         {
             rb.velocity *= grVelocityAmplifier;
 
-            if (rb.velocity.magnitude<0.5f) // now hard coded..
+            
+            if (rb.velocity.magnitude<0.5f) // now hard coded.. // TODO: state machine. Only do this code when currently rolling
             {
                 StandupStraight();
             }
@@ -61,6 +75,7 @@ public class Jojo : Character
         Vector3 movementDir = orientation.forward * verticalInput + orientation.right * horizontalinput;
 
         //if (movementDir != Vector3.zero)
+
             //rb.transform.forward = Vector3.Slerp(rb.transform.forward, movementDir.normalized, Time.deltaTime * rotationSpeed); //rotate the player parent directly, not the individual character children
 
         rb.velocity += movementDir * acceleration;
