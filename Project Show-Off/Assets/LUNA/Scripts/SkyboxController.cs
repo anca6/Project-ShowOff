97c59bbe15b2882f04355cb0886eb62c250907a3
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2024-06-03 12:46:52 +0200

    Set global shader vector in LateUpdate

--- Project Show-Off/Assets/LUNA/Scripts/SkyboxController.cs
+++ Project Show-Off/Assets/LUNA/Scripts/SkyboxController.cs
@@ -2,6 +2,12 @@
     using UnityEngine;
     public class SkyboxController : MonoBehaviour{
         public Transform Sun;
+        
         private static readonly int SunDirection = Shader.PropertyToID("_SunDirection");
+
+        private void LateUpdate(){
+            if (Sun == null) return;
+            Shader.SetGlobalVector(SunDirection, -Sun.transform.forward);
+        }
     }
 }
