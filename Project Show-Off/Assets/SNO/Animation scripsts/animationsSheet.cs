83acc60e55e6bbd0f2e2dd12fb6120b90e8a08ca
SnoYK <527806@student.saxion.nl>
2024-06-07 14:50:50 +0200

    testing and debugging

--- Project Show-Off/Assets/SNO/Animation scripsts/animationsSheet.cs
+++ Project Show-Off/Assets/SNO/Animation scripsts/animationsSheet.cs
@@ -6,13 +6,14 @@ using UnityEngine.InputSystem;
 
 public class animationsSheet : MonoBehaviour
 {
-    
+
 
     [SerializeField] private Animator sarahAnim;
     [SerializeField] protected LayerMask isGround;
     [SerializeField] protected float playerHeight;
     [SerializeField] private bool groundOverwrite = false;
-   [SerializeField] private PlayerControls _actions;
+
+    [SerializeField] private PlayerControls _actions;
     private bool moving = false;
     private bool grounded;
     private bool ability;
@@ -22,35 +23,45 @@ public class animationsSheet : MonoBehaviour
     private void Awake()
     {
         _actions = new PlayerControls();
-        _actions.Gameplay1.Movement.performed += ctx => moving = ctx.ReadValueAsButton();
-        _actions.Gameplay1.Movement.canceled += ctx => moving = ctx.ReadValueAsButton();
-        _actions.Gameplay1.Ability.performed += ctx => ability = ctx.ReadValueAsButton();
-        _actions.Gameplay1.Ability.canceled += ctx => ability = ctx.ReadValueAsButton();
-        _actions.Gameplay1.Jump.performed += ctx => jump = ctx.ReadValueAsButton();
-        _actions.Gameplay1.Jump.canceled += ctx => jump = ctx.ReadValueAsButton();
+        //_actions.Gameplay1.Movement.performed += ctx => moving = ctx.ReadValueAsButton();
 
-
+        // _actions.Gameplay1.Movement.canceled += ctx => moving = ctx.ReadValueAsButton();
         
+          _actions.Gameplay1.Ability.performed += ctx => ability = ctx.ReadValueAsButton();
+          _actions.Gameplay1.Ability.canceled += ctx => ability = ctx.ReadValueAsButton();
+        /*
+          _actions.Gameplay1.Jump.performed += ctx => jump = ctx.ReadValueAsButton();
+          _actions.Gameplay1.Jump.canceled += ctx => jump = ctx.ReadValueAsButton();
+        */
+
+
     }
 
+    private void OnEnable()
+    {
+        _actions.Gameplay1.Movement.Enable();
+        _actions.Gameplay1.Ability.Enable();
+        _actions.Gameplay1.Jump.Enable();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-      
-       
 
+
+        //if no object is dragged in warn and return
         if (sarahAnim == null)
         {
             Debug.LogWarning("Sarah Animator not set on:" + gameObject);
             return;
         }
+        //if playerheight is below 0 warn
         if (playerHeight < 0)
         {
             Debug.LogWarning("PlayerHeight <0 " + gameObject + "will forever fall");
         }
 
-       
+
         sarahAnim = GetComponent<Animator>();
 
     }
@@ -61,9 +72,9 @@ public class animationsSheet : MonoBehaviour
     {
 
 
-
+        //if the moving bool is true then sett walk to true in the animator controller
         if (moving)
-  
+
         {
             sarahAnim.SetBool("Walk", true);
             Debug.Log(gameObject + "Walk set to:" + moving);
@@ -74,24 +85,25 @@ public class animationsSheet : MonoBehaviour
             //Debug.Log(gameObject + "Walk set to:" + moving);
         }
 
-        if(ability)
+        //if ability is true then sett ability to true in the animator controller
+        if (ability)
         {
-            sarahAnim.SetBool("Ability", true) ;
-            Debug.Log(gameObject + "Ability set to: true");
-        }
-        else sarahAnim.SetBool("Ability", false) ;
+            sarahAnim.SetBool("Ability", true);
 
+        }
+        else sarahAnim.SetBool("Ability", false);
+
+        //if jump is true and grounded is true sett jump in the animator contorller to true
         if (jump && grounded)
         {
 
             sarahAnim.SetBool("Jump", true);
 
-            Debug.DrawRay(transform.position, Vector3.down * (playerHeight * 0.5f + 0.3f));
             Debug.Log(gameObject + "Jump set to: true");
         }
-        else sarahAnim.SetBool("Jump", false) ;
+        else sarahAnim.SetBool("Jump", false);
 
-
+        //if the player is not grounded set float to true in the animator controller
         if (!grounded)
         {
             sarahAnim.SetBool("Float", true);
@@ -102,21 +114,23 @@ public class animationsSheet : MonoBehaviour
         }
 
 
-       
 
-        if ( Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, playerHeight * 0.5f + 0.3f, isGround) && groundOverwrite == false)
-      {
+        //check if the player is grounded or not
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, playerHeight * 0.5f + 0.3f, isGround) && groundOverwrite == false)
+        {
             grounded = true;
         }
         else
         {
             grounded = false;
-            Debug.DrawRay(transform.position, Vector3.down * (playerHeight * 0.5f +0.3f), Color.red);
+
+            //if they're not grounded draw a ray same lenght as the one wich checks for ground.
+            Debug.DrawRay(transform.position, Vector3.down * (playerHeight * 0.5f + 0.3f), Color.red);
         }
-      
-       // Debug.Log("Grounded sett to:" + Grounded);
+
+
     }
-  
-    
-   
+
+
+
 }
