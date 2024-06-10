bef0e1063e5b303811edc7be9438c27e221477cb
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2024-06-10 10:40:59 +0200

    Simplify logic

--- Project Show-Off/Assets/SNO/Animation scripsts/PlayerAnimationLink.cs
+++ Project Show-Off/Assets/SNO/Animation scripsts/PlayerAnimationLink.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 public class PlayerAnimationLink : MonoBehaviour
 {
-    
-
     [SerializeField] private Animator Anim;
     [SerializeField] protected LayerMask isGround;
     [SerializeField] protected float playerHeight;
@@ -38,79 +36,31 @@ public class PlayerAnimationLink : MonoBehaviour
     {
 
         // if the player presses W,A,S,D then this means the player is moving.
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            moving = true;
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S) ||
+            Input.GetKey(KeyCode.W)) moving = true;
 
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            moving = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            moving = true;
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            moving = true;
-        }
-
-        if (Input.GetKey(KeyCode.A) == false &&(Input.GetKey(KeyCode.D) == false) && (Input.GetKey(KeyCode.S) == false) && Input.GetKey(KeyCode.W) == false)
-        {
-            moving = false;
-        }
-
-
-
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.S) &&
+            !Input.GetKey(KeyCode.W)) moving = false;
       
         //if w,a,s or d is pressed toggle the "Walk" bool in the animator to on
-        if (moving)
-  
-        {
-            Anim.SetBool("Walk", true);
-            Debug.Log(gameObject + "Walk set to:" + moving);
-        }
-        //toggle it off
-        else if (!moving)
-        {
-            Anim.SetBool("Walk", false);
-            //Debug.Log(gameObject + "Walk set to:" + moving);
-        }
+        Anim.SetBool("Walk", moving);
+        Debug.Log(gameObject + "Walk set to:" + moving);
 
         // if hte player presses F toggle teh "ability" bool in the animator on
-        if(Input.GetKey(KeyCode.F))
-        {
-            Anim.SetBool("Ability", true) ;
-            Debug.Log(gameObject + "Ability set to: true");
-        }
-        //toggle it off
-        else Anim.SetBool("Ability", false) ;
+        Anim.SetBool("Ability", Input.GetKey(KeyCode.F)) ;
+        Debug.Log(gameObject + "Ability set to: " + Anim.GetBool("Ability"));
 
         //if space is pressed and the entity is grounded putt the jump bool to true
-        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
+        bool jumping = Input.GetKeyDown(KeyCode.Space) && Grounded;
+        Anim.SetBool("Jump", jumping);
+        if (jumping)
         {
-
-            Anim.SetBool("Jump", true);
-
             Debug.DrawRay(transform.position, Vector3.down * (playerHeight * 0.5f + 0.3f));
             Debug.Log(gameObject + "Jump set to: true");
         }
-        //putt it to off
-        else Anim.SetBool("Jump", false) ;
 
         //if the entity is not grounded toggle the floating bool int he animator to on
-        if (!Grounded)
-        {
-            Anim.SetBool("Float", true);
-        }
-        //toggle it off
-        else
-        {
-            Anim.SetBool("Float", false);
-        }
-
-
+        Anim.SetBool("Float", !Grounded);
         
         //check if the entity is half the playerHeight above an object with a collider wich is also on the layer isGround.
         //if it is set the bool grounded to true
@@ -124,12 +74,8 @@ public class PlayerAnimationLink : MonoBehaviour
         {
             Grounded = false;
             Debug.DrawRay(transform.position, Vector3.down * (playerHeight * 0.5f + 0.3f), Color.red);
-
         }
       
         // Debug.Log("Grounded sett to:" + Grounded);
     }
-  
-    
-   
 }
