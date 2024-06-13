8cc8e726a60a37b3df18eab9f316e179687d6625
anca6 <526100@student.saxion.nl>
2024-06-13 17:57:34 +0200

    Progress on character selection

--- Project Show-Off/Assets/ANCA/Scrips/Character Selection/CharacterSelection.cs
+++ Project Show-Off/Assets/ANCA/Scrips/Character Selection/CharacterSelection.cs
@@ -4,51 +4,22 @@ using UnityEngine.UI;
 
 public class CharacterSelection : MonoBehaviour
 {
-    [SerializeField] private GameObject[] player1Characters;
-    [SerializeField] private GameObject[] player2Characters;
+    [SerializeField] private string gameScene;
 
-    private int selectedCharacterIndexPlayer1 = 0;
-    private int selectedCharacterIndexPlayer2 = 0;
+    private int player1Selection = 0;
+    private int player2Selection = 0;
 
-    [SerializeField] private Button[] player1Buttons;
-    [SerializeField] private Button[] player2Buttons;
-
-    [SerializeField] private string sceneName;
-
-    void Start()
+    private void SelectCharacterP1(int index)
     {
-        UpdateSelectionUI();
+        player1Selection = index;
     }
-
-    public void SelectCharacterPlayer1(int index)
+    private void SelectCharacterP2(int index)
     {
-        selectedCharacterIndexPlayer1 = index;
-        UpdateSelectionUI();
+        player2Selection = index;
     }
-
-    public void SelectCharacterPlayer2(int index)
+    private void StartGame()
     {
-        selectedCharacterIndexPlayer2 = index;
-        UpdateSelectionUI();
-    }
-
-    void UpdateSelectionUI()
-    {
-        for (int i = 0; i < player1Buttons.Length; i++)
-        {
-            player1Buttons[i].interactable = i != selectedCharacterIndexPlayer1;
-        }
-
-        for (int i = 0; i < player2Buttons.Length; i++)
-        {
-            player2Buttons[i].interactable = i != selectedCharacterIndexPlayer2;
-        }
-    }
-
-    public void StartGame()
-    {
-        PlayerPrefs.SetInt("SelectedCharacterPlayer1", selectedCharacterIndexPlayer1);
-        PlayerPrefs.SetInt("SelectedCharacterPlayer2", selectedCharacterIndexPlayer2);
-        SceneManager.LoadScene(sceneName);
+        GameManager.instance.SetPlayerSelection(player1Selection, player2Selection);
+        SceneManager.LoadScene(gameScene);
     }
 }
