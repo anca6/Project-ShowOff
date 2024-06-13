0100844f15159015df8586409f08945fd67c347b
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2024-06-13 14:52:35 +0200

    Remove update method

--- Project Show-Off/Assets/LUNA/Scripts/ScreenNormalsRendering.cs
+++ Project Show-Off/Assets/LUNA/Scripts/ScreenNormalsRendering.cs
@@ -5,9 +5,5 @@ namespace LUNA.Scripts{
         // Start is called before the first frame update
         private void Start(){
         }
-
-        // Update is called once per frame
-        private void Update(){
-        }
     }
 }
