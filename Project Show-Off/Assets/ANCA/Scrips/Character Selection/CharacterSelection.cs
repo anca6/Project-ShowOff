105d8223333b94a502f313a79fcd20d729ac715a
anca6 <526100@student.saxion.nl>
2024-06-13 19:57:50 +0200

    Basic character selection

--- Project Show-Off/Assets/ANCA/Scrips/Character Selection/CharacterSelection.cs
+++ Project Show-Off/Assets/ANCA/Scrips/Character Selection/CharacterSelection.cs
@@ -9,15 +9,15 @@ public class CharacterSelection : MonoBehaviour
     private int player1Selection = 0;
     private int player2Selection = 0;
 
-    private void SelectCharacterP1(int index)
+    public void SelectCharacterP1(int index)
     {
         player1Selection = index;
     }
-    private void SelectCharacterP2(int index)
+    public void SelectCharacterP2(int index)
     {
         player2Selection = index;
     }
-    private void StartGame()
+    public void StartGame()
     {
         GameManager.instance.SetPlayerSelection(player1Selection, player2Selection);
         SceneManager.LoadScene(gameScene);
