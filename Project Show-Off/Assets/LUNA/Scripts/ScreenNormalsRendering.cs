f5505d91ab00870351d3cf823314e8993b303a78
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2024-06-13 14:54:21 +0200

    Add ScreenNormalsShader public variable

--- Project Show-Off/Assets/LUNA/Scripts/ScreenNormalsRendering.cs
+++ Project Show-Off/Assets/LUNA/Scripts/ScreenNormalsRendering.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 namespace LUNA.Scripts{
     [RequireComponent(typeof(Camera))]
     public class ScreenNormalsRendering : MonoBehaviour{
+        public Shader ScreenNormalsShader;
+        
         // Start is called before the first frame update
         private void Start(){
         }
