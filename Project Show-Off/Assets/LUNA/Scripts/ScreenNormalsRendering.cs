042af44c8b95c730fc1efe416902eda85f793d1a
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2024-06-13 14:51:18 +0200

    Make ScreenNormalsRendering require a Camera to be attached

--- Project Show-Off/Assets/LUNA/Scripts/ScreenNormalsRendering.cs
+++ Project Show-Off/Assets/LUNA/Scripts/ScreenNormalsRendering.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 namespace LUNA.Scripts{
+    [RequireComponent(typeof(Camera))]
     public class ScreenNormalsRendering : MonoBehaviour{
         // Start is called before the first frame update
         private void Start(){
