0298dad6b605fa570983a4c7c13051e2987e88ba
anca6 <526100@student.saxion.nl>
2024-06-14 03:25:15 +0200

    Update on character selection - start game only if both players selected a character to play as

--- Project Show-Off/Assets/ANCA/Scrips/Character Selection/CharacterSelection.cs
+++ Project Show-Off/Assets/ANCA/Scrips/Character Selection/CharacterSelection.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,21 +6,51 @@ using UnityEngine.UI;
 public class CharacterSelection : MonoBehaviour
 {
     [SerializeField] private string gameScene;
+    [SerializeField] private Button startButton;
+    [SerializeField] private TextMeshProUGUI feedbackText;
+    [SerializeField] private string feedbackMessage;
 
-    private int player1Selection = 0;
-    private int player2Selection = 0;
+    private int player1Selection = -1;
+    private int player2Selection = -1;
+    private void Start()
+    {
+        UpdateStartButtonState();
+    }
 
     public void SelectCharacterP1(int index)
     {
         player1Selection = index;
+        UpdateStartButtonState();
     }
     public void SelectCharacterP2(int index)
     {
         player2Selection = index;
+        UpdateStartButtonState();
+    }
+
+    private void UpdateStartButtonState()
+    {
+        if (player1Selection >= 0 && player2Selection >= 0)
+        {
+            startButton.interactable = true;
+            feedbackText.text = "";
+        }
+        else
+        {
+            startButton.interactable = false;
+            feedbackText.text = feedbackMessage;
+        }
     }
     public void StartGame()
     {
-        GameManager.instance.SetPlayerSelection(player1Selection, player2Selection);
-        SceneManager.LoadScene(gameScene);
+        if (player1Selection >= 0 && player2Selection >= 0)
+        {
+            GameManager.instance.SetPlayerSelection(player1Selection, player2Selection);
+            SceneManager.LoadScene(gameScene);
+        }
+        else
+        {
+            feedbackText.text = feedbackMessage;
+        }
     }
 }
