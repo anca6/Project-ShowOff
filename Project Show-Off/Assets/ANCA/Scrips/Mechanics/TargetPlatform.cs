f176bef10b2e76834f3950618bc0dd41fb9de328
anca6 <526100@student.saxion.nl>
2024-06-14 02:23:46 +0200

    Breakable platform done

--- Project Show-Off/Assets/ANCA/Scrips/Mechanics/TargetPlatform.cs
+++ Project Show-Off/Assets/ANCA/Scrips/Mechanics/TargetPlatform.cs
@@ -51,4 +51,20 @@ public class TargetPlatform : MonoBehaviour
     {
         isMoving = false;
     }
+
+  /*  private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }*/
 }
