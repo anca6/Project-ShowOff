76095af98a3e81b81b29fda1d75d140fe769b15f
anca6 <526100@student.saxion.nl>
2024-06-14 02:23:46 +0200

    Breakable platform done

---
+++ Project Show-Off/Assets/ANCA/Scrips/Mechanics/BreakablePlatform.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakablePlatform : MonoBehaviour
+{
+    [SerializeField] private float breakDelay;
+    [SerializeField] private float respawnTime;
+
+    private Collider platformCollider;
+    private Renderer platformRenderer;
+
+    private void Start()
+    {
+        platformCollider = GetComponent<Collider>();
+        platformRenderer = GetComponent<Renderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(BreakPlatform());
+        }
+    }
+
+    private IEnumerator BreakPlatform()
+    {
+        // Wait for the specified break delay
+        yield return new WaitForSeconds(breakDelay);
+
+        // Disable the platform by disabling the Collider and Renderer
+        platformCollider.enabled = false;
+        platformRenderer.enabled = false;
+
+        // Wait for the specified respawn time
+        yield return new WaitForSeconds(respawnTime);
+
+        // Re-enable the platform by enabling the Collider and Renderer
+        platformCollider.enabled = true;
+        platformRenderer.enabled = true;
+    }
+}
