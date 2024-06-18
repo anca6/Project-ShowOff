2014cce175a2d0e112ea979ffe2506876a03d853
SnoYK <527806@student.saxion.nl>
2024-06-18 16:05:13 +0200

    Revert "Merge branch 'designer' into artist"
    This reverts commit 7e049d3a781f402c0f53c40d9b7064f9e9e54d33, reversing
    changes made to 5f6921d3e0538a5580e1c23d8761f05393dacd35.

--- Project Show-Off/Assets/ANCA/Scrips/Character Selection/CharacterSelection.cs
+++ Project Show-Off/Assets/ANCA/Scrips/Character Selection/CharacterSelection.cs
@@ -1,6 +1,5 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -11,90 +10,22 @@ public class CharacterSelection : MonoBehaviour
     [SerializeField] private TextMeshProUGUI feedbackText;
     [SerializeField] private string feedbackMessage;
 
-    [SerializeField] private GameObject[] player1Images;
-    [SerializeField] private GameObject[] player2Images;
-    [SerializeField] private GameObject[] player1HoverImages;
-    [SerializeField] private GameObject[] player2HoverImages;
-    [SerializeField] private GameObject HighlightStartbuttonImage;
-
-    private int player1Selection = 0; // Default to the first option for Player 1
-    private int player2Selection = 0; // Default to the first option for Player 2
-
+    private int player1Selection = -1;
+    private int player2Selection = -1;
     private void Start()
     {
-        UpdateSelectionImages(player1Images, player1Selection, player1HoverImages);
-        UpdateSelectionImages(player2Images, player2Selection, player2HoverImages);
         UpdateStartButtonState();
     }
 
-    private void Update()
+    public void SelectCharacterP1(int index)
     {
-        HandlePlayer1Input();
-        HandlePlayer2Input();
+        player1Selection = index;
+        UpdateStartButtonState();
     }
-
-    private void HandlePlayer1Input()
+    public void SelectCharacterP2(int index)
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            MoveSelectionUp(ref player1Selection, player1Images.Length);
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            MoveSelectionDown(ref player1Selection, player1Images.Length);
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            SelectCharacterP1(player1Selection);
-        }
-    }
-
-    private void HandlePlayer2Input()
-    {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            MoveSelectionUp(ref player2Selection, player2Images.Length);
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            MoveSelectionDown(ref player2Selection, player2Images.Length);
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            SelectCharacterP2(player2Selection);
-        }
-    }
-
-
-    private void MoveSelectionUp(ref int selection, int maxIndex)
-    {
-        selection--;
-        if (selection < 0)
-        {
-            selection = maxIndex - 1; // Make it Loop back to the last option
-        }
-        UpdateSelectionImages(player1Images, player1Selection, player1HoverImages);
-        UpdateSelectionImages(player2Images, player2Selection, player2HoverImages);
-    }
-
-    private void MoveSelectionDown(ref int selection, int maxIndex)
-    {
-        selection++;
-        if (selection >= maxIndex)
-        {
-            selection = 0; // Make it Loop back to the first option
-        }
-        UpdateSelectionImages(player1Images, player1Selection, player1HoverImages);
-        UpdateSelectionImages(player2Images, player2Selection, player2HoverImages);
-    }
-
-    private void UpdateSelectionImages(GameObject[] images, int selectedIndex, GameObject[] hoverImages)
-    {
-        for (int i = 0; i < images.Length; i++)
-        {
-            images[i].SetActive(i == selectedIndex);
-            hoverImages[i].SetActive(i == selectedIndex);
-        }
+        player2Selection = index;
+        UpdateStartButtonState();
     }
 
     private void UpdateStartButtonState()
@@ -110,33 +41,6 @@ public class CharacterSelection : MonoBehaviour
             feedbackText.text = feedbackMessage;
         }
     }
-
-    public void SelectCharacterP1(int index)
-    {
-        player1Selection = index;
-        UpdateSelectionImages(player1Images, player1Selection, player1HoverImages);
-        UpdateStartButtonState();
-
-        // Set the startButton as the selected game object
-        EventSystem.current.SetSelectedGameObject(startButton.gameObject);
-
-        // Activate the selection indicator image
-        HighlightStartbuttonImage.SetActive(true);
-    }
-
-    public void SelectCharacterP2(int index)
-    {
-        player2Selection = index;
-        UpdateSelectionImages(player2Images, player2Selection, player2HoverImages);
-        UpdateStartButtonState();
-
-        // Set the startButton as the selected game object
-        EventSystem.current.SetSelectedGameObject(startButton.gameObject);
-
-        // Activate the selection indicator image
-        HighlightStartbuttonImage.SetActive(true);
-    }
-
     public void StartGame()
     {
         if (player1Selection >= 0 && player2Selection >= 0)
