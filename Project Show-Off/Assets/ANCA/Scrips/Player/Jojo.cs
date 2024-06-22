404d8689e00ef9b159697869e831150d09eee1c2
Luke-design-cloud <135381597+Luke-design-cloud@users.noreply.github.com>
2024-06-22 22:51:48 +0200

    Convert mp3 to wav and add breakable sound

--- Project Show-Off/Assets/ANCA/Scrips/Player/Jojo.cs
+++ Project Show-Off/Assets/ANCA/Scrips/Player/Jojo.cs
@@ -10,7 +10,8 @@ public class Jojo : Character
     [SerializeField] private float explosionDelay = 0.5f;
 
     bool StandupFinished = true;
-
+    public AudioClip clip2;
+    
     protected override void Start()
     {
         rb = GetComponentInParent<Rigidbody>();
@@ -111,6 +112,7 @@ public class Jojo : Character
 
     private IEnumerator DelayedExplosion(GameObject breakableObject)
     {
+        source.PlayOneShot(clip2);
         yield return new WaitForSeconds(explosionDelay);
         TriggerExplosion(transform.position);
         Destroy(breakableObject);
