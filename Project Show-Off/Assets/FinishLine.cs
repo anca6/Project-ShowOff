ff0497f8c2fd76b50883e5247cd49d43be6f403a
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2024-06-26 04:14:51 +0200

    Add basic OnTriggerEnter method

--- Project Show-Off/Assets/FinishLine.cs
+++ Project Show-Off/Assets/FinishLine.cs
@@ -1,4 +1,9 @@
 using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class FinishLine : MonoBehaviour{
+    public string PlayerTag = "Player";
+    
+    private void OnTriggerEnter(Collider other){
+        if (!other.gameObject.CompareTag(PlayerTag)) return;
+    }
 }
